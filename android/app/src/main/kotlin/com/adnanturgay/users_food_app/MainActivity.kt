83c5cb8eaffabc6aed4c84ef588818fdd543d766
Thanh Duy Nguyen <duythanh1565@gmail.com>
2022-10-26 07:18:26 +0700

    update

---
+++ android/app/src/main/kotlin/com/adnanturgay/users_food_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.adnanturgay.users_food_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,49 @@
-
-import 'package:ecommerce_int2/screens/splash_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:users_food_app/assistantMethods/address_changer.dart';
+import 'package:users_food_app/assistantMethods/cart_item_counter.dart';
+import 'package:users_food_app/assistantMethods/total_amount.dart';
+import 'package:users_food_app/firebase_options.dart';
+import 'global/global.dart';
+import 'splash_screen/splash_screen.dart';
 
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-
-void main() => runApp(MyApp());
+  sharedPreferences = await SharedPreferences.getInstance();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'eCommerce int2',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.light,
-        canvasColor: Colors.transparent,
-        primarySwatch: Colors.blue,
-        fontFamily: "Montserrat",
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: ((c) => CartItemCounter()),
+        ),
+        ChangeNotifierProvider(
+          create: ((c) => TotalAmount()),
+        ),
+        ChangeNotifierProvider(
+          create: ((c) => AddressChanger()),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Users App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.amber,
+        ),
+        home: const SplashScreen(),
       ),
-      home: SplashScreen(),
     );
   }
 }
