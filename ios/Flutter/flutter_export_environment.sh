dd0459d9a4b131dc62bd614e08c963bbfbc03d36
Thanh Duy Nguyen <duythanh1565@gmail.com>
2022-11-06 14:21:25 +0700

    udate

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=E:\Program file\flutter"
+export "FLUTTER_ROOT=C:\Users\duyth\fvm\versions\3.3.4"
 export "FLUTTER_APPLICATION_PATH=D:\study\UN_4th\E-commerce\mobile_client\TrippleT_CF_client"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
